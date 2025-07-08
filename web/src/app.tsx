43f8919769b2112dfa763270e122433833347645
edurodrigues0 <edusacramento9@gmail.com>
2025-07-08 16:55:28 -0300

    feat: integrate @tanstack/react-query for data fetching

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { CreateRoom } from "./pages/create-room";
 import { Room } from "./pages/room";
+import { queryClient } from "./lib/react-query";
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<CreateRoom />} />
-        <Route element={<Room />} path="/room" />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<CreateRoom />} />
+          <Route element={<Room />} path="/room" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
