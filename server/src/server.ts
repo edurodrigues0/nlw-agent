2961e837813771aba381eeec2b44d4e0e453ce1c
edurodrigues0 <edusacramento9@gmail.com>
2025-07-08 08:29:21 -0300

    add get rooms route

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,7 @@ import {
 } from "fastify-type-provider-zod";
 import { env } from "./env.ts";
 import { sql } from "./db/connection.ts";
+import { getRoomsRoute } from "./http/routes/get-rooms.ts";
 
 const app = fastify();
 
@@ -20,6 +21,8 @@ app.setValidatorCompiler(validatorCompiler);
 app.get("/health", () => {
   return "ok";
 });
+
+app.register(getRoomsRoute);
 
 app.listen({ port: env.PORT }).then(() => {
   console.log("HTTP server running on port 3333");
