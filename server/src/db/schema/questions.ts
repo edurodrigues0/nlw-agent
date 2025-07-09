c501f54f849d094bdaf72cb81b9a9a66eacf0cd2
edurodrigues0 <edusacramento9@gmail.com>
2025-07-09 09:32:23 -0300

    feat: add questions table and schema definition

---
+++ server/src/db/schema/questions.ts
@@ -0,0 +1,16 @@
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { rooms } from "./rooms.ts";
+
+export const questions = pgTable("questions", {
+  id: uuid().primaryKey().defaultRandom(),
+  roomId: uuid()
+    .references(() => rooms.id)
+    .notNull(),
+  question: text().notNull(),
+  answer: text(),
+  createdAt: timestamp().defaultNow().notNull(),
+  updatedAt: timestamp()
+    .defaultNow()
+    .notNull()
+    .$onUpdate(() => new Date()),
+});
